2319a3e750d0c7c2228f91a82a808c49823d67a3
sappenin <sappenin@gmail.com>
2018-05-05 17:15:22 -0600

    Fixes #13
    * Add unit tests to validate how Ed25519 Fulfillments with varying messages operate inside of prefix conditions with varying prefixes.
    
    Signed-off-by: sappenin <sappenin@gmail.com>

--- crypto-conditions/src/test/java/com/ripple/cryptoconditions/PrefixSha256FulfillmentTest.java
+++ crypto-conditions/src/test/java/com/ripple/cryptoconditions/PrefixSha256FulfillmentTest.java
@@ -9,9 +9,9 @@ package com.ripple.cryptoconditions;
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -29,8 +29,17 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import static org.junit.Assert.assertTrue;
 
 import com.ripple.cryptoconditions.helpers.TestFulfillmentFactory;
+import com.ripple.cryptoconditions.helpers.TestKeyFactory;
+import net.i2p.crypto.eddsa.EdDSAEngine;
+import net.i2p.crypto.eddsa.EdDSAPublicKey;
 import org.junit.Test;
 
+import java.security.InvalidKeyException;
+import java.security.KeyPair;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.security.Signature;
+import java.security.SignatureException;
 import java.util.Base64;
 import java.util.UUID;
 
@@ -41,12 +50,11 @@ public class PrefixSha256FulfillmentTest extends AbstractCryptoConditionTest {
 
   private static final String PREFIX = "when this baby hits 88 miles per hour";
   private static final String PREFIX2 = "Nobody calls me chicken!";
-  private static final String ENCODED_PREFIX
-      = "d2hlbiB0aGlzIGJhYnkgaGl0cyA4OCBtaWxlcyBwZXIgaG91cg==";
+  private static final String ENCODED_PREFIX = "d2hlbiB0aGlzIGJhYnkgaGl0cyA4OCBtaWxlcyBwZXIgaG91cg==";
   private static final String ENCODED_FINGERPRINT = "-28EVNr7rOwQ_XsvrJVxLvjBY38ZNZlHaPHYpsIbmH4";
 
   private static final PreimageSha256Fulfillment SUBFULFILLMENT = TestFulfillmentFactory
-      .constructPreimageFulfillment(PREIMAGE1);
+    .constructPreimageFulfillment(PREIMAGE1);
 
   /**
    * Tests concurrently creating an instance of {@link RsaSha256Fulfillment}. This test validates the fix for Github
@@ -59,10 +67,9 @@ public class PrefixSha256FulfillmentTest extends AbstractCryptoConditionTest {
   public void testConstructionUsingMultipleThreads() throws Exception {
     final Runnable runnableTest = () -> {
       final PrefixSha256Fulfillment preimageSha256Fulfillment =
-          constructPrefixSha256Fulfillment(UUID.randomUUID().toString());
+        constructPrefixSha256Fulfillment(UUID.randomUUID().toString());
       assertThat(preimageSha256Fulfillment.getType(), is(CryptoConditionType.PREFIX_SHA256));
-      assertThat(preimageSha256Fulfillment.verify(preimageSha256Fulfillment.getDerivedCondition()),
-          is(true));
+      assertThat(preimageSha256Fulfillment.verify(preimageSha256Fulfillment.getDerivedCondition()), is(true));
     };
 
     // Run single-threaded...
@@ -93,28 +100,71 @@ public class PrefixSha256FulfillmentTest extends AbstractCryptoConditionTest {
 
   @Test
   public final void testValidateDerivedCondition() {
-    final PrefixSha256Fulfillment actual = TestFulfillmentFactory
-        .constructPrefixSha256Fulfillment(PREFIX);
-    assertTrue("Invalid condition", actual.verify(actual.getDerivedCondition(), new byte[] {}));
+    final PrefixSha256Fulfillment actual = TestFulfillmentFactory.constructPrefixSha256Fulfillment(PREFIX);
+    assertTrue("Invalid condition", actual.verify(actual.getDerivedCondition(), new byte[]{}));
+  }
+
+  /**
+   * <p>Validates Issue 12 to ensure that a prefix fulfillment/condition is correct when the <tt>prefix</tt> matches
+   * the contained sub-condition's <tt>message</tt> value.</p>
+   *
+   * @see "https://github.com/ripple/crypto-conditions/issues/12"
+   */
+  @Test
+  public final void testSubconditionMessageMatchesPrefix() {
+    final String message = "Marty! I need you to go back with me!";
+    final String prefix = "Back to the Future!";
+
+    // Validate that a message that matches the prefix works...
+    this.testSubconditionMessageCombinations(message, message);
+    // Validate that a message that does not match the prefix works...
+    this.testSubconditionMessageCombinations(message, prefix);
+    // Validate that an empty message works...
+    this.testSubconditionMessageCombinations("", prefix);
+    // Validate that a an empty prefix works...
+    this.testSubconditionMessageCombinations(message, "");
+    // Validate that an empty message and an empty prefix works...
+    this.testSubconditionMessageCombinations("", "");
+  }
+
+  /**
+   * Helper method to support {@link #testSubconditionMessageMatchesPrefix()}.
+   */
+  private void testSubconditionMessageCombinations(final String message, final String prefix) {
+    try {
+      final KeyPair ed25519KeyPair = TestKeyFactory.generateRandomEd25519KeyPair();
+      final MessageDigest sha512Digest = MessageDigest.getInstance("SHA-512");
+      final Signature edDsaSigner = new EdDSAEngine(sha512Digest);
+      edDsaSigner.initSign(ed25519KeyPair.getPrivate());
+      edDsaSigner.update((prefix + message).getBytes());
+      final byte[] edDsaSignature = edDsaSigner.sign();
+
+      final Ed25519Sha256Fulfillment signatureFulfillment = Ed25519Sha256Fulfillment
+        .from((EdDSAPublicKey) ed25519KeyPair.getPublic(), edDsaSignature);
+
+      // Construct a Prefix Fulfillment
+      final PrefixSha256Fulfillment prefixFulfillment = PrefixSha256Fulfillment.from(
+        prefix.getBytes(), prefix.length() + message.length(), signatureFulfillment
+      );
+      final PrefixSha256Condition prefixCondition = prefixFulfillment.getDerivedCondition();
+      assertTrue(prefixFulfillment.verify(prefixCondition, message.getBytes()));
+    } catch (NoSuchAlgorithmException | SignatureException | InvalidKeyException e) {
+      throw new RuntimeException(e);
+    }
   }
 
   @Test
   public final void testValidateDerivedConditionWithEmptyMessage() {
-    final PrefixSha256Fulfillment actual = TestFulfillmentFactory
-        .constructPrefixSha256Fulfillment(PREFIX);
+    final PrefixSha256Fulfillment actual = TestFulfillmentFactory.constructPrefixSha256Fulfillment(PREFIX);
     assertTrue("Invalid condition", actual.verify(actual.getDerivedCondition()));
   }
 
   @Test
   public void testGettersAndSetters() {
-    final PrefixSha256Fulfillment actual = TestFulfillmentFactory
-        .constructPrefixSha256Fulfillment(PREFIX);
+    final PrefixSha256Fulfillment actual = TestFulfillmentFactory.constructPrefixSha256Fulfillment(PREFIX);
 
-    assertThat(actual.getSubfulfillment(),
-        is(TestFulfillmentFactory.constructPreimageFulfillment(PREIMAGE1))
-    );
-    assertThat(actual.getPrefixBase64Url(),
-        is("d2hlbiB0aGlzIGJhYnkgaGl0cyA4OCBtaWxlcyBwZXIgaG91cg=="));
+    assertThat(actual.getSubfulfillment(), is(TestFulfillmentFactory.constructPreimageFulfillment(PREIMAGE1)));
+    assertThat(actual.getPrefixBase64Url(), is("d2hlbiB0aGlzIGJhYnkgaGl0cyA4OCBtaWxlcyBwZXIgaG91cg=="));
     assertThat(actual.getPrefix(), is(Base64.getDecoder().decode(actual.getPrefixBase64Url())));
     assertThat(actual.getType(), is(CryptoConditionType.PREFIX_SHA256));
     assertThat(actual.getDerivedCondition(), is(not(nullValue())));
@@ -122,12 +172,9 @@ public class PrefixSha256FulfillmentTest extends AbstractCryptoConditionTest {
 
   @Test
   public void equalsHashcode() {
-    final PrefixSha256Fulfillment fulfillment1
-        = TestFulfillmentFactory.constructPrefixSha256Fulfillment(PREFIX);
-    final PrefixSha256Fulfillment fulfillment2
-        = TestFulfillmentFactory.constructPrefixSha256Fulfillment(PREFIX);
-    final PrefixSha256Fulfillment fulfillment3
-        = TestFulfillmentFactory.constructPrefixSha256Fulfillment(PREFIX2);
+    final PrefixSha256Fulfillment fulfillment1 = TestFulfillmentFactory.constructPrefixSha256Fulfillment(PREFIX);
+    final PrefixSha256Fulfillment fulfillment2 = TestFulfillmentFactory.constructPrefixSha256Fulfillment(PREFIX);
+    final PrefixSha256Fulfillment fulfillment3 = TestFulfillmentFactory.constructPrefixSha256Fulfillment(PREFIX2);
 
     assertThat(fulfillment1.equals(fulfillment1), is(true));
     assertThat(fulfillment2.equals(fulfillment2), is(true));
@@ -148,26 +195,25 @@ public class PrefixSha256FulfillmentTest extends AbstractCryptoConditionTest {
 
   @Test
   public void testToString() {
-    final PrefixSha256Fulfillment fulfillment
-        = TestFulfillmentFactory.constructPrefixSha256Fulfillment(PREFIX);
+    final PrefixSha256Fulfillment fulfillment = TestFulfillmentFactory.constructPrefixSha256Fulfillment(PREFIX);
 
     assertThat(fulfillment.toString(),
-        is("PrefixSha256Fulfillment{"
-            + "prefix=" + ENCODED_PREFIX + ", "
-            + "maxMessageLength=1000, "
-            + "subfulfillment=PreimageSha256Fulfillment{"
-            + "encodedPreimage=Um9hZHM_IFdoZXJlIHdlJ3JlIGdvaW5nLCB3ZSBkb24ndCBuZWVkIHJvYWRzLg==, "
-            + "type=PREIMAGE-SHA-256, "
-            + "derivedCondition=PreimageSha256Condition{"
-            + "type=PREIMAGE-SHA-256, "
-            + "fingerprint=" + ENCODED_FINGERPRINT + ", "
-            + "cost=46}}, "
-            + "type=PREFIX-SHA-256, "
-            + "derivedCondition=PrefixSha256Condition{"
-            + "subtypes=[PREIMAGE-SHA-256], "
-            + "type=PREFIX-SHA-256, "
-            + "fingerprint=2ugoaAzCSomLbveq9nNmSJp5X-esBSjBw5IGFgvYF9w, "
-            + "cost=2107"
-            + "}}"));
+      is("PrefixSha256Fulfillment{"
+        + "prefix=" + ENCODED_PREFIX + ", "
+        + "maxMessageLength=1000, "
+        + "subfulfillment=PreimageSha256Fulfillment{"
+        + "encodedPreimage=Um9hZHM_IFdoZXJlIHdlJ3JlIGdvaW5nLCB3ZSBkb24ndCBuZWVkIHJvYWRzLg==, "
+        + "type=PREIMAGE-SHA-256, "
+        + "derivedCondition=PreimageSha256Condition{"
+        + "type=PREIMAGE-SHA-256, "
+        + "fingerprint=" + ENCODED_FINGERPRINT + ", "
+        + "cost=46}}, "
+        + "type=PREFIX-SHA-256, "
+        + "derivedCondition=PrefixSha256Condition{"
+        + "subtypes=[PREIMAGE-SHA-256], "
+        + "type=PREFIX-SHA-256, "
+        + "fingerprint=2ugoaAzCSomLbveq9nNmSJp5X-esBSjBw5IGFgvYF9w, "
+        + "cost=2107"
+        + "}}"));
   }
 }
