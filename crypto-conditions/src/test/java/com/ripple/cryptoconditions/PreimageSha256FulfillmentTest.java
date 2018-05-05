7495195db49b53bfbdb4f87bd7067c6a6371333d
David Fuelling <sappenin@gmail.com>
2018-05-05 15:44:25 -0700

    Fixes #13 (#15)
    * Add unit tests to verify that PreimageSHA-256 conditions/fulfillments can be created and verified with small , as well as long, preimages.
    
    Signed-off-by: sappenin <sappenin@gmail.com>

--- crypto-conditions/src/test/java/com/ripple/cryptoconditions/PreimageSha256FulfillmentTest.java
+++ crypto-conditions/src/test/java/com/ripple/cryptoconditions/PreimageSha256FulfillmentTest.java
@@ -9,9 +9,9 @@ package com.ripple.cryptoconditions;
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -30,6 +30,7 @@ import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
+import java.security.SecureRandom;
 import java.util.Base64;
 import java.util.UUID;
 
@@ -38,117 +39,123 @@ import java.util.UUID;
  */
 public class PreimageSha256FulfillmentTest extends AbstractCryptoConditionTest {
 
-  private static final String PREIMAGE = "when this baby hits 88 miles per hour";
-  private static final String PREIMAGE2 = "Nobody calls me chicken!";
-  private static final String ENCODED_PREIMAGE
-      = "d2hlbiB0aGlzIGJhYnkgaGl0cyA4OCBtaWxlcyBwZXIgaG91cg==";
-  private static final String ENCODED_FINGERPRINT = "iL1xV1F0IvtokoaU1n2eVOvcwhy4me4vroUKg8vFnOE";
-  private static final byte[] FINGERPRINT_BYTES = Base64.getUrlDecoder()
-      .decode(ENCODED_FINGERPRINT);
+    private static final String PREIMAGE = "when this baby hits 88 miles per hour";
+    private static final String PREIMAGE2 = "Nobody calls me chicken!";
+    private static final String ENCODED_PREIMAGE = "d2hlbiB0aGlzIGJhYnkgaGl0cyA4OCBtaWxlcyBwZXIgaG91cg==";
+    private static final String ENCODED_FINGERPRINT = "iL1xV1F0IvtokoaU1n2eVOvcwhy4me4vroUKg8vFnOE";
+    private static final byte[] FINGERPRINT_BYTES = Base64.getUrlDecoder().decode(ENCODED_FINGERPRINT);
 
-  private static final PreimageSha256Condition TEST_CONDITION
-      = PreimageSha256Condition.fromCostAndFingerprint(
-      37,
-      FINGERPRINT_BYTES
-  );
+    private static final PreimageSha256Condition TEST_CONDITION = PreimageSha256Condition.fromCostAndFingerprint(
+        37,
+        FINGERPRINT_BYTES
+    );
 
-  /**
-   * Tests concurrently creating an instance of {@link PreimageSha256Fulfillment}. This test validates the fix for
-   * Github issue #40 where construction of this class was not thread-safe.
-   *
-   * @see "https://github.com/interledger/java-crypto-conditions/issues/40"
-   * @see "https://github.com/junit-team/junit4/wiki/multithreaded-code-and-concurrency"
-   */
-  @Test
-  public void testConstructionUsingMultipleThreads() throws Exception {
-    final Runnable runnableTest = () -> {
-      final PreimageSha256Fulfillment preimageSha256Fulfillment =
-          constructPreimageFulfillment(UUID.randomUUID().toString());
-      assertThat(preimageSha256Fulfillment.getType(), is(CryptoConditionType.PREIMAGE_SHA256));
-      assertThat(preimageSha256Fulfillment.verify(preimageSha256Fulfillment.getDerivedCondition()),
-          is(true));
-    };
+    /**
+     * Tests concurrently creating an instance of {@link PreimageSha256Fulfillment}. This test validates the fix for
+     * Github issue #40 where construction of this class was not thread-safe.
+     *
+     * @see "https://github.com/interledger/java-crypto-conditions/issues/40"
+     * @see "https://github.com/junit-team/junit4/wiki/multithreaded-code-and-concurrency"
+     */
+    @Test
+    public void testConstructionUsingMultipleThreads() throws Exception {
+        final Runnable runnableTest = () -> {
+            final PreimageSha256Fulfillment preimageSha256Fulfillment =
+                constructPreimageFulfillment(UUID.randomUUID().toString());
 
-    // Run single-threaded...
-    this.runConcurrent(1, runnableTest);
-    // Run multi-threaded...
-    this.runConcurrent(runnableTest);
-  }
+            assertThat(preimageSha256Fulfillment.getType(), is(CryptoConditionType.PREIMAGE_SHA256));
+            assertThat(preimageSha256Fulfillment.verify(preimageSha256Fulfillment.getDerivedCondition()),
+                is(true));
+        };
+
+        // Run single-threaded...
+        this.runConcurrent(1, runnableTest);
+        // Run multi-threaded...
+        this.runConcurrent(runnableTest);
+    }
 
 
-  @Test(expected = NullPointerException.class)
-  public final void testNullPreimage() {
-    PreimageSha256Fulfillment.from(null);
-  }
+    @Test(expected = NullPointerException.class)
+    public final void testNullPreimage() {
+        PreimageSha256Fulfillment.from(null);
+    }
 
-  @Test
-  public final void testGetCondition() {
-    final PreimageSha256Fulfillment fulfillment
-        = PreimageSha256Fulfillment.from(PREIMAGE.getBytes());
-    assertEquals("Wrong condition", TEST_CONDITION, fulfillment.getDerivedCondition());
-  }
+    /**
+     * Test preimage lengths from 0 to 100.
+     */
+    @Test
+    public final void testSmallToLargePreimage() {
+        for (int PREIMAGE_LENGTH = 0; PREIMAGE_LENGTH < 100; PREIMAGE_LENGTH++) {
+            // Fill the array with PREIMAGE_LENGTH bytes...
+            final byte[] preimageBytes = new byte[PREIMAGE_LENGTH];
+            new SecureRandom().nextBytes(preimageBytes);
 
-  @Test
-  public final void testValidate() {
-    final PreimageSha256Fulfillment actual
-        = PreimageSha256Fulfillment.from(PREIMAGE.getBytes());
-    assertTrue("Invalid condition", actual.verify(TEST_CONDITION, new byte[] {}));
-  }
+            final PreimageSha256Fulfillment fulfillment = PreimageSha256Fulfillment.from(preimageBytes);
+            assertThat(fulfillment.getDerivedCondition().getCost(), is(Long.valueOf(PREIMAGE_LENGTH)));
+            assertTrue("Invalid condition", fulfillment.verify(fulfillment.getDerivedCondition(), new byte[]{}));
+        }
+    }
 
-  @Test
-  public final void testValidateWithEmptyMessage() {
-    final PreimageSha256Fulfillment actual
-        = PreimageSha256Fulfillment.from(PREIMAGE.getBytes());
-    assertTrue("Invalid condition", actual.verify(TEST_CONDITION));
-  }
+    @Test
+    public final void testGetCondition() {
+        final PreimageSha256Fulfillment fulfillment = PreimageSha256Fulfillment.from(PREIMAGE.getBytes());
+        assertEquals("Wrong condition", TEST_CONDITION, fulfillment.getDerivedCondition());
+    }
 
-  @Test
-  public void testGettersAndSetters() {
-    final PreimageSha256Fulfillment actual
-        = PreimageSha256Fulfillment.from(PREIMAGE.getBytes());
-    assertThat(actual.getEncodedPreimage(), is(ENCODED_PREIMAGE));
-    assertThat(actual.getType(), is(CryptoConditionType.PREIMAGE_SHA256));
-    assertThat(actual.getDerivedCondition(), is(not(nullValue())));
-  }
+    @Test
+    public final void testValidate() {
+        final PreimageSha256Fulfillment actual = PreimageSha256Fulfillment.from(PREIMAGE.getBytes());
+        assertTrue("Invalid condition", actual.verify(TEST_CONDITION, new byte[]{}));
+    }
 
-  @Test
-  public void equalsHashcode() {
-    final PreimageSha256Fulfillment fulfillment1
-        = PreimageSha256Fulfillment.from(PREIMAGE.getBytes());
-    final PreimageSha256Fulfillment fulfillment2
-        = PreimageSha256Fulfillment.from(PREIMAGE.getBytes());
-    final PreimageSha256Fulfillment fulfillment3
-        = PreimageSha256Fulfillment.from(PREIMAGE2.getBytes());
+    @Test
+    public final void testValidateWithEmptyMessage() {
+        final PreimageSha256Fulfillment actual = PreimageSha256Fulfillment.from(PREIMAGE.getBytes());
+        assertTrue("Invalid condition", actual.verify(TEST_CONDITION));
+    }
 
-    assertThat(fulfillment1.equals(fulfillment1), is(true));
-    assertThat(fulfillment2.equals(fulfillment2), is(true));
-    assertThat(fulfillment3.equals(fulfillment3), is(true));
+    @Test
+    public void testGettersAndSetters() {
+        final PreimageSha256Fulfillment actual = PreimageSha256Fulfillment.from(PREIMAGE.getBytes());
+        assertThat(actual.getEncodedPreimage(), is(ENCODED_PREIMAGE));
+        assertThat(actual.getType(), is(CryptoConditionType.PREIMAGE_SHA256));
+        assertThat(actual.getDerivedCondition(), is(not(nullValue())));
+    }
 
-    assertThat(fulfillment1.equals(fulfillment2), is(true));
-    assertThat(fulfillment1.equals(fulfillment3), is(false));
+    @Test
+    public void equalsHashcode() {
+        final PreimageSha256Fulfillment fulfillment1 = PreimageSha256Fulfillment.from(PREIMAGE.getBytes());
+        final PreimageSha256Fulfillment fulfillment2 = PreimageSha256Fulfillment.from(PREIMAGE.getBytes());
+        final PreimageSha256Fulfillment fulfillment3 = PreimageSha256Fulfillment.from(PREIMAGE2.getBytes());
 
-    assertThat(fulfillment2.equals(fulfillment1), is(true));
-    assertThat(fulfillment2.equals(fulfillment3), is(false));
+        assertThat(fulfillment1.equals(fulfillment1), is(true));
+        assertThat(fulfillment2.equals(fulfillment2), is(true));
+        assertThat(fulfillment3.equals(fulfillment3), is(true));
 
-    assertThat(fulfillment3.equals(fulfillment1), is(false));
-    assertThat(fulfillment3.equals(fulfillment2), is(false));
+        assertThat(fulfillment1.equals(fulfillment2), is(true));
+        assertThat(fulfillment1.equals(fulfillment3), is(false));
 
-    assertThat(fulfillment1.hashCode(), is(fulfillment2.hashCode()));
-    assertThat(fulfillment1.hashCode() == fulfillment3.hashCode(), is(false));
-  }
+        assertThat(fulfillment2.equals(fulfillment1), is(true));
+        assertThat(fulfillment2.equals(fulfillment3), is(false));
 
-  @Test
-  public void testToString() {
-    final PreimageSha256Fulfillment fulfillment
-        = PreimageSha256Fulfillment.from(PREIMAGE.getBytes());
+        assertThat(fulfillment3.equals(fulfillment1), is(false));
+        assertThat(fulfillment3.equals(fulfillment2), is(false));
 
-    assertThat(fulfillment.toString(),
-        is("PreimageSha256Fulfillment{"
-            + "encodedPreimage=d2hlbiB0aGlzIGJhYnkgaGl0cyA4OCBtaWxlcyBwZXIgaG91cg==, "
-            + "type=PREIMAGE-SHA-256, "
-            + "derivedCondition=PreimageSha256Condition{type=PREIMAGE-SHA-256, "
-            + "fingerprint=iL1xV1F0IvtokoaU1n2eVOvcwhy4me4vroUKg8vFnOE,"
-            + " cost=37"
-            + "}}"));
-  }
+        assertThat(fulfillment1.hashCode(), is(fulfillment2.hashCode()));
+        assertThat(fulfillment1.hashCode() == fulfillment3.hashCode(), is(false));
+    }
+
+    @Test
+    public void testToString() {
+        final PreimageSha256Fulfillment fulfillment = PreimageSha256Fulfillment.from(PREIMAGE.getBytes());
+
+        assertThat(fulfillment.toString(),
+            is("PreimageSha256Fulfillment{"
+                + "encodedPreimage=d2hlbiB0aGlzIGJhYnkgaGl0cyA4OCBtaWxlcyBwZXIgaG91cg==, "
+                + "type=PREIMAGE-SHA-256, "
+                + "derivedCondition=PreimageSha256Condition{type=PREIMAGE-SHA-256, "
+                + "fingerprint=iL1xV1F0IvtokoaU1n2eVOvcwhy4me4vroUKg8vFnOE,"
+                + " cost=37"
+                + "}}"));
+    }
 }
